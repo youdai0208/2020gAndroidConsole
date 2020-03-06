c5a209663e33b69aa2b6b04ff471ca8a6a13501c
youdai0208 <yotto.robocon@gmail.com>
2020-03-06 14:48:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yotto/usb_host_test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.yotto.usb_host_test;
+
+import android.hardware.usb.UsbManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text_view;
+    private UsbManager usb_manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
